fb948d6af5154b93596b26621fdc8a9adcdc9c0c
danielboyle <danielboyle111@gmail.com>
2015-04-16 18:19:30 -0400

    Final

--- js/app.js
+++ js/app.js
@@ -23,8 +23,22 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		//Ryu goes back to ready position
 	});
+});
+
+$(document).keydown(function(e) {
+	if (e.keyCode === 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-cool').show();
+	};
+});
+
+$(document).keyup(function(e) {
+	if (e.keyCode === 88) {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	}
 });
 
 function playHadouken() {
